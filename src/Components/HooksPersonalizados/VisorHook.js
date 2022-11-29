a1efd1c64cb365d8b5f8a1dd7bb30b1568f6e52f
teotronic5438 <elias.orihuela>
2022-11-28 22:23:23 -0300

    video 20 - custom hooks personalizadas final fetch

--- src/Components/HooksPersonalizados/VisorHook.js
+++ src/Components/HooksPersonalizados/VisorHook.js
@@ -1,34 +1,15 @@
-import React, { useState } from 'react';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-// import { useFetch } from '../../hooks/useFetch';
+import React from 'react';
 
 export default function VisorHook(props) {
     // const [datapokemon, setDataPokemon] = useState([])
-    const pokemones = props.data.results.map((element) => {
-        let pokemon;
-        fetch(element.url)
-        .then(response => response.json())
-        .then(json => {
-            pokemon = {
-                id: json.id,
-                name: json.name,
-                avatar: json.sprites.front_default,
-            }
-
-            // setDataPokemon((datapokemon) => [...datapokemon, pokemon]);
-        })
-        .catch(error => console.error(error));
-        // console.log(pokemon);
-        return pokemon;
-    });
+    const pokemones = props.data.results.map((element, index) => <div>
+        <p className='text-success' key={index}>Pokemon: {element.name}</p>
+        <p className='text-primary'>URL: {element.url}</p>
+    </div>);
 
     return (
         <>
-            <Row xs={1} md={2} className="g-2">
-                {/* {pokemones && {pokemones} } */}
-            </Row>
+            {pokemones}
         </>
 
     )
